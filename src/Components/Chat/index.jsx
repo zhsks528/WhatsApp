d7ae020fdd228c9aa5a1a01b06d8dfb940f76f39
SeongMin Han <hsm950825@naver.com>
2021-01-17 17:24:50 +0900

    Add: Chat Header

--- src/Components/Chat/index.jsx
+++ src/Components/Chat/index.jsx
@@ -1,6 +1,34 @@
 import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+
 import "./Chat.css";
 
-const Chat = () => <div className="chat">b</div>;
+const Chat = () => (
+  <div className="chat">
+    <div className="chat__header">
+      <Avatar />
+
+      <div className="chat__headerInfo">
+        <div>Room name</div>
+        <span>Last seen at...</span>
+      </div>
+
+      <div className="chat__headerRight">
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <IconButton>
+          <AttachFileIcon />
+        </IconButton>
+        <IconButton>
+          <MoreVertIcon />
+        </IconButton>
+      </div>
+    </div>
+  </div>
+);
 
 export default Chat;
