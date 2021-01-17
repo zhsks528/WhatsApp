4886c01e80cb616177b4216d72db8f9d01183a4e
SeongMin Han <hsm950825@naver.com>
2021-01-17 16:57:21 +0900

    Add: Sidebar Search Box

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -3,6 +3,7 @@ import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import { Avatar, IconButton } from "@material-ui/core";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
 import "./Sidebar.css";
 
 const Sidebar = () => (
@@ -19,6 +20,13 @@ const Sidebar = () => (
         <IconButton>
           <MoreVertIcon />
         </IconButton>
+      </div>
+    </div>
+
+    <div className="sidebar__search">
+      <div className="sidebar__searchContainer">
+        <SearchIcon />
+        <input type="text" placeholder="Search or start new chat" />
       </div>
     </div>
   </div>
