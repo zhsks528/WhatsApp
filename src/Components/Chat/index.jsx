580773f50c0dcc7ad00d7f1832dc2e71f0aefe69
SeongMin Han <hsm950825@naver.com>
2021-01-17 17:36:37 +0900

    Add: Chat Body

--- src/Components/Chat/index.jsx
+++ src/Components/Chat/index.jsx
@@ -28,6 +28,24 @@ const Chat = () => (
         </IconButton>
       </div>
     </div>
+
+    <div className="chat__body">
+      <p className="chat__message">
+        <span className="chat__name">Sonny</span>
+        This is a message
+        <span className="chat__timestamp">{new Date().toUTCString()}</span>
+      </p>
+      <p className="chat__message chat__receiver">
+        <span className="chat__name">Sonny</span>
+        This is a message
+        <span className="chat__timestamp">{new Date().toUTCString()}</span>
+      </p>
+      <p className="chat__message">
+        <span className="chat__name">Sonny</span>
+        This is a message
+        <span className="chat__timestamp">{new Date().toUTCString()}</span>
+      </p>
+    </div>
   </div>
 );
 
